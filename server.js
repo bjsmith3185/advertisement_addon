c6b121519916094d9887b0ee77f32442601013ef
bjsmith3185 <bjsmith3185@hotmail.com>
2018-12-04 18:50:50 -0500

    added to server.js

--- server.js
+++ server.js
@@ -12,6 +12,18 @@ app.use(bodyParser.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+
+
+
+
+
 // Add routes, both API and view
 app.use(routes);
 
@@ -21,4 +33,11 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/usersearches");
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
+
+
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
